a8d5422809e72636358a18b8f32615dbfed794db
Pekka Laaksonen <pekka.laaksonen@nordcloud.com>
2022-11-17 11:19:25 +0200

    changed stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
-DIR="var/www/html"
-FILE="index.php"
-
-
-if [ -f "$DIR"/"$FILE" ]; then
+if [ -f "/var/www/html/index.php" ]; then
  	echo "Wordpress installation found."
-	if [ -f "wordpress/latest.tar.gz" ]; then
-		echo "Removing Wordpress archive..." && rm latest.tar.gz
-	fi
 else
-	echo "No Wordpress installation found. Downloading the archive..."
+	echo "No Wordpress installation found. Downloading the archive from wordpress.org."
  	curl -O https://wordpress.org/latest.tar.gz
- 	echo "Archive downloaded... extracting contents..."
+ 	#echo "Done" 
+	echo "Extracting contents..."
 	tar -xzf latest.tar.gz
-	#echo "Creating directory var/www/html"
-	#mkdir -p var/www/html
-	echo "Moving Wordpress..."
-	mv wordpress/* var/www/html
- 	echo "Removing archive and folder..."
+	#echo "Done."
+	echo "Moving files to /var/www/html..."
+	mv wordpress/* /var/www/html
+	#echo "Done."
+ 	echo "Cleaning up Wordpress archive etc..."
 	rm -r wordpress
  	rm latest.tar.gz
-	echo "All done!"
+	#echo "Done."
 fi
 
 exec "$@"
